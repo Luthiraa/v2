65ecba2f18f7d40a6b889e5e6e23664a05c9c601
Luthira A <79228258+Luthiraa@users.noreply.github.com>
2025-05-18 19:48:18 -0400

    Replace favicon with a PNG file and remove the old ICO file from the project.

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favico.ico" />
+        <link rel="icon" href="/favico.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
